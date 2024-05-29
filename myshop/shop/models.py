27de81b97abf1fc299d7b35cac731fdaa46f0c93
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-29 20:39:09 +0300

    adding get_abosolute_url for both category and products

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=200)
     slug=models.SlugField(max_length=200)
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category",args=[self.slug])
+    
 
     class Meta:
         ordering=('name',)
@@ -21,6 +25,9 @@ class Product(models.Model):
     available=models.BooleanField(default=True)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
+    def get_absolute_url(self):
+        return reverse("shop:product_detail",args=[self.id,self.slug])
+    
     class Meta:
         ordering=('name',)
         indexes=[
