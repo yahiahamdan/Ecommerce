c62ed4e81f79709433b6d7103da7ea751c422e1e
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-30 18:41:53 +0300

    modifiying the url

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('shop/', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     
