96ec09cf0f66b020f89c140188837b039694b425
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-30 18:38:35 +0300

    modifiying the settings

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -27,10 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-MEDIA_URL = 'media/'
+import os
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 
 # Application definition
 
@@ -59,7 +58,7 @@ ROOT_URLCONF = 'myshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
