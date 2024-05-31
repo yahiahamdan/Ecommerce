5084f87971287ba2f548719d3a4e30849fe9bf4d
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-31 19:01:14 +0300

    views to handle cart methods

---
+++ myshop/cart/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect,get_object_or_404
+
+# Create your views here.
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddPrdouctForm
+
+@require_POST
+def cart_add(request,product_id):
+    print("cart add view called")
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddPrdouctForm(request.POST)
+    if form.is_valid():
+      cd = form.cleaned_data
+      print("form is valid")
+      cart.add(product=product,
+          quantity=cd['quantity'],
+          override_quantity=cd['override'])
+      return redirect('cart:cart_detail')
+    else:
+        print("form is invalid",form.errors)
+        return redirect('cart:cart_detail')
+
+
+def cart_remove(request,product_id):
+    cart=Cart(request)
+    product=get_object_or_404(Product,id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart=Cart(request)
+    totalPrice= cart.getTotalPrice()
+    return render(request,'cart/detail.html',{'cart':cart,'totalPrice':totalPrice})
