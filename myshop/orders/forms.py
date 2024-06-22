b33d410263ee02b78ada44af74c7b3731dde6a83
yahia hamdan <hamdanyahia2@gmail.com>
2024-06-22 21:02:24 +0300

    updating the forms in orders

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import Order
-class OrderCreateform(forms.ModelForm):
+class OrderCreateForm(forms.ModelForm):
     class Meta:
         model=Order
-        fields=['first_name','last_name','email','address','city']
-        
+        fields=['first_name','last_name','phone','email','address','city']
