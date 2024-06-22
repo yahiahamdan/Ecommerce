0812390bd27e037df1a4357fc84d3baa490e72ff
yahia hamdan <hamdanyahia2@gmail.com>
2024-06-22 21:01:28 +0300

    modifying the modesl in the orders

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from shop.models import Product
+from django.core import validators
 # Create your models here.
 class Order(models.Model):
  first_name = models.CharField(max_length=50)
  last_name=models.CharField(max_length=50)
+ phone=models.CharField(max_length=11,
+                        validators=[validators.RegexValidator(r'^\d{11}$')]) 
  email=models.EmailField()
- postal_code=models.CharField(max_length=20)
+ address=models.CharField(max_length=300)
  city=models.CharField(max_length=100)
  created=models.DateTimeField(auto_now_add=True)
  updated=models.DateTimeField(auto_now=True)
@@ -20,6 +23,7 @@ class Order(models.Model):
     return f'Order {self.id}'
 def get_total_cost(self):
     return sum(item.get_cost() for item in self.items.all())
+
 class OrderItem(models.Model):
  order=models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
  product=models.ForeignKey(Product,related_name='order_items',on_delete=models.CASCADE)
